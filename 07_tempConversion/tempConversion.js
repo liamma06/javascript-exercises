2057582e19da9ee3cb3d3c4505f88a6ce230055e
liamma06 <liam.jbr.ma@gmail.com>
2024-08-29 23:19:33 +0800

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let celsiusResult = Math.round(((fahren - 32) * 5/9)*10) / 10;
+  return celsiusResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenResult = Math.round((celsius*9/5 + 32)*10) / 10;
+  return fahrenResult;
 };
 
 // Do not edit below this line
