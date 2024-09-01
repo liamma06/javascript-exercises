7fe85dda7e0dd5360f8421fb1f5f64c573590e50
liamma06 <liam.jbr.ma@gmail.com>
2024-09-02 05:11:57 +0800

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(first,second) {
+  return(first+second);
 };
 
-const subtract = function() {
-	
+const subtract = function(first,second) {
+	return(first-second);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum,current)=> current + sum,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multi,current)=> multi*current,1);
 };
 
-const power = function() {
-	
+const power = function(first,second) {
+	return Math.pow(first,second);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0) return 1;
+  
+  let result = 1;
+
+  for(i = num; i > 0; i --){
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
